172cbf3946bed8969a576931d61f3a7e2de8180b
Hridyanshu <hridyanshusharma@gmail.com>
2019-03-18 10:59:04 +0530

    3 Files with basic cout statements

---
+++ f1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello programmers"<<endl;
+	return 0;
+}
---
+++ f3.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"This time we will do subtraction"<<endl;
+	return 0;
+}
+
