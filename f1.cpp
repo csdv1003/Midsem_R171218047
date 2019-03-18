8e63fa07542d3a614286e88b366d15be0061b9b3
Hridyanshu <hridyanshusharma@gmail.com>
2019-03-18 11:02:58 +0530

    Declared two variables each in f1 and f3, f2 unchanged

--- f1.cpp
+++ f1.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
+	int a,b;
 	cout<<"Hello programmers"<<"Lets do multiplication of two numbers"<<endl;
 	return 0;
 }
--- f3.cpp
+++ f3.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
+	int a,b;
 	cout<<"This time we will do subtraction"<<endl;
 	return 0;
 }
