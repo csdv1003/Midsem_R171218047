742718403f40696ed14d01d18c65cba4f1a88c98
Hridyanshu <hridyanshusharma@gmail.com>
2019-03-18 10:42:16 +0530

    Functions defined

--- calculator.cpp
+++ calculator.cpp
@@ -3,8 +3,29 @@ class calculator
 {
 	public:
 		int a,b;
-		int add();
-		int diff;
-		int mul();
-		int div(); //Functions declared only
-};
+		int add()
+		{
+			int sum;
+			sum=a+b;
+			return sum;
+		}
+		int diff()
+		{
+			int dif;
+			dif=a-b;
+			return dif;
+		}
+		int mul()
+		{
+			int prod;
+			prod=a*b;
+			return prod;
+		}
+		int div()
+		{
+			int quot;
+			quot=a/b;
+			return quot;
+		}
+			
+};	//Added function definitions
