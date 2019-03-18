084917494bb95b321f3bd7b0942c87d2eb1cb37a
Hridyanshu <hridyanshusharma@gmail.com>
2019-03-18 10:52:47 +0530

    Completed the code

--- calculator.cpp
+++ calculator.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+using namespace std;
 class calculator
 {
 	public:
@@ -29,3 +30,32 @@ class calculator
 		}
 			
 };	//Added function definitions
+
+int main()
+{
+	calculator obj;
+	cout<<"Please enter two numbers"<<endl;
+	cin>>obj.a>>obj.b;
+	int s,d,p,q;
+	cout<<"Enter 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division"<<endl;
+	int choice;
+	cin>>choice;
+	switch (choice)
+	{
+		case 1: s=obj.add();
+			cout<<"Sum= "<<s<<endl;
+			break;
+		case 2: d=obj.diff();
+			cout<<"Difference= "<<d<<endl;
+			break;
+		case 3: p=obj.mul();
+			cout<<"Product= "<<p<<endl;
+			break;
+		case 4: q=obj.div();
+			cout<<"Division= "<<1<<endl;
+			break;
+		default: cout<<"Please enter a valid choice"<<endl;
+	}
+	return 0;
+}  //Added main function
+
